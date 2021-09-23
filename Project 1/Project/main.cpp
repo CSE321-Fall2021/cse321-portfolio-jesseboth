f79042ba424f7c9309ecc953ab26dcce828f2711
jesseboth <jessebot@buffalo.edu>
2021-09-22 20:29:06 -0400

    Initial Commit

---
+++ Project 1/Project/main.cpp
@@ -0,0 +1,29 @@
+/**
+ * Author: Jesse Both
+ * Date:
+ * 
+ * Assignment:
+ * 
+ * Purpose:   
+ * 
+ * Inputs: 
+ * Outputs:
+ * 
+ * Constraints:
+ * 
+ * Subroutines:
+ * 
+ * Sources:
+ * 
+*/
+
+#include "mbed.h"
+
+// main() runs in its own thread in the OS
+int main()
+{
+    while (true) {
+
+    }
+}
+
