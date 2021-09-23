49ebb9ab61aca13b0fa27eefb234351d1fdd77e4
jesseboth <jessebot@buffalo.edu>
2021-09-23 18:15:48 -0400

    Blinky LED with switch

--- Project 1/Project/main.cpp
+++ Project 1/Project/main.cpp
@@ -1,13 +1,14 @@
 /**
  * Author: Jesse Both
- * Date:
+ * Date: 9/23/2021
  * 
- * Assignment:
+ * Assignment:...
  * 
- * Purpose:   
+ * Purpose:   Activates PA_1 to turn on an LED
+ *            with the push of a button in PB_1
  * 
- * Inputs: 
- * Outputs:
+ * Inputs:  Button - PB_1
+ * Outputs: LED - PA_1
  * 
  * Constraints:
  * 
@@ -22,8 +23,26 @@
 // main() runs in its own thread in the OS
 int main()
 {
+    RCC->AHB2ENR |= 0x3;        //enable port A and B
+
+    /*output*/
+    GPIOA->MODER &= ~(0x8);     //set bit 1 to 0 PA1[1]
+    GPIOA->MODER |= 0x4;        //set bit 2 to 1 PA1[0]
+
+    /*input*/
+    GPIOB->MODER &= ~(0xC);     //set bit 2 and 3 to 0 PB1[1:0]
+
     while (true) {
 
+        /*check if button is pressed*/
+        if((GPIOB->IDR & 0x2) != 0x2){
+            GPIOA->ODR |= 0x2;      //activate LED         
+        }
+        else{
+            GPIOA->ODR &= ~(0x2);   //deactivate LED
+        }
+
+        thread_sleep_for(500);
     }
 }
 
