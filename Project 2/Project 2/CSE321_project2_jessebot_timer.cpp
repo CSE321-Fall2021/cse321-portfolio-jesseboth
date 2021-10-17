8604cf2b25a5f391f93054fdafc480d2d61aac31
jesseboth <jessebot@buffalo.edu>
2021-10-16 20:55:50 -0400

    Added additional timer manipulation features

--- Project 2/Project 2/CSE321_project2_jessebot_timer.cpp
+++ Project 2/Project 2/CSE321_project2_jessebot_timer.cpp
@@ -25,7 +25,12 @@
 struct timer{
     int minutes;
     int seconds;
-    char str[6]; // mm:ss
+    int inc_by = 0;     // default 0
+    char str[6];        // mm:ss
+    char min_goal = 0;  // default 0
+    char sec_goal = 0;  // default 0 
+    char press[5];      // mss
+    char press_i = 0;
 } timer;
 
 
@@ -46,33 +51,28 @@ struct timer get_timer(){
 }
 
 /* increments the timer by input
-input:
-        by (incrementation value)
+
 */
-void inc_timer(int by){
-    timer.seconds += by;        //inc seconds 
+void inc_timer(){
+    timer.seconds += timer.inc_by;        //inc seconds 
     
     //check if minutes should be increased.
     if(timer.seconds == 60){
-        timer.minutes += by;
+        timer.minutes += timer.inc_by;
         timer.seconds = 0;
     }
-    else if(timer.seconds == 0){
-        timer.minutes += by;
+    else if(timer.seconds == -1){
+        timer.minutes += timer.inc_by;
         timer.seconds = 59;
     }
-    
 }
 
 /* check if goal is reached
-input:
-        m (minutes)
-        s (seconds)
 output: 
         goal reached t/f
 */
-int goal_timer(int m, int s){
-    if(timer.minutes == m && timer.seconds == s){
+int goal_timer(){
+    if(timer.minutes == timer.min_goal && timer.seconds == timer.sec_goal){
         return true;
     }
     return false;
@@ -107,3 +107,62 @@ char *string_timer(){
     return timer.str;
 }
 
+/* sets the value for incrementation 
+intput:
+        by - +1 or -1
+*/
+void set_inc_by_timer(int by){
+    timer.inc_by = by;
+}
+
+void set_press_timer(char c){
+    if(timer.press_i < 3){
+        timer.press[timer.press_i++] = c;
+    }
+}
+
+void reset_press_timer(){
+    while(timer.press_i > 0){
+        timer.press[timer.press_i--] = 0;
+    }
+}
+
+void decode_timer(){
+    if(strlen(timer.press) == 3){
+        timer.str[0] = timer.press[0];
+        timer.minutes = string_to_int(timer.str);
+        timer.str[1] = ':';
+        timer.str[2] = timer.press[1];
+        timer.str[3] = timer.press[2];
+        timer.seconds = string_to_int(timer.str+2);
+        
+    }
+    else{
+        timer.str[0] = timer.press[0];
+        timer.str[1] = timer.press[1];
+        timer.minutes = string_to_int(timer.str);
+        timer.str[2] = ':';
+        timer.str[3] = timer.press[2];
+        timer.str[4] = timer.press[3];
+        timer.seconds = string_to_int(timer.str+3);
+    }
+}
+
+int string_to_int(char *str){
+    int mult = 1;
+    char *cpy = str-1; 
+    str += string_length(str)-1;
+    int ret = 0;
+    while(str != cpy){
+        ret += (*str - '0')*mult;
+        mult*=10;
+        str--;
+    }
+    return ret;
+}
+
+int string_length(char *str){
+    int i;
+    for(i=0; *str != 0; i++, str++);
+    return i;
+}
--- Project 2/Project 2/CSE321_project2_jessebot_timer.h
+++ Project 2/Project 2/CSE321_project2_jessebot_timer.h
@@ -1,8 +1,15 @@
+#include "mbed.h"
+
 struct timer;
 
 void set_timer(int m, int s);
 struct timer get_timer();
-void inc_timer(int by);
-int goal_timer(int m, int s);
+void inc_timer();
+int goal_timer();
 char *string_timer();
-
+void set_inc_by_timer(int by);
+void set_press_timer(char c);
+void reset_press_timer();
+void decode_timer();
+int string_to_int(char *str);
+int string_length(char *str);
