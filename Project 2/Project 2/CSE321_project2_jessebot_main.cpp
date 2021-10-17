33783b4733725fb71d565f2457d6c04be3e5caae
jesseboth <jessebot@buffalo.edu>
2021-10-17 14:44:04 -0400

    Timer aspect is fully working

--- Project 2/Project 2/CSE321_project2_jessebot_main.cpp
+++ Project 2/Project 2/CSE321_project2_jessebot_main.cpp
@@ -61,17 +61,18 @@ char timer_flag;
 char press_flag = 0;    // allows key press to function when set
 char press_pause = 0;   // delays keypress
 char row = 0;           // row selection set
-char timer_cursor = 6;
 char digit_num = 0;
 char pressed = 0;
 char new_state = 1;
-char state = 0;
 int inc_by = -1;       // default to decrease
+char c_flag = 0;
+char state = 0;
 /*   state:
         0 - do nothing wait for 'D'
-        1 - input digits m:ss
-        2 - wait for 'A' and 'C'
+        1 - input digits m:ss, wait for A or C
+        2 - paused
         3 - count down - check for 'B'
+        4 - make LEDs blink;
 */
 
 void timer_isr(){
@@ -87,10 +88,9 @@ void column0_isr(){
         press_flag = 0;
         char c = get_char_keypad(row, 0);
 
-        if(state == 1 && c != '*' && (digit_num != 1 || (digit_num == 1 && c < '6'))){
+        if(state == 1 && c != '*'){
                 set_press_timer(c);
                 set_key(c);
-            
         }
     }
 }
@@ -100,7 +100,7 @@ void column1_isr(){
         press_flag = 0;
         char c = get_char_keypad(row, 1);
 
-        if(state == 1 && (digit_num != 1 || (digit_num == 1 && c < '6'))){
+        if(state == 1){
             set_press_timer(c);
             set_key(c);
         }
@@ -112,7 +112,7 @@ void column2_isr(){
         press_flag = 0;
         char c = get_char_keypad(row, 2);
     
-        if(state == 1 && c != '#' && (digit_num != 1 || (digit_num == 1 && c < '6'))){
+        if(state == 1 && c != '#'){
             set_press_timer(c);
             set_key(c);
         }
@@ -127,19 +127,26 @@ void column3_isr(){
         if(state == 0 && c == 'D'){
             state = 1;
             new_state = 1;
-            timer_cursor = 6;
             pressed = 0;
         }
-        else if(state == 2){
-            if(c == 'C'){}          //TODO
+        else if(state == 1 || state == 2){
+            if(c == 'C' && state == 1){
+                c_flag = 1;
+                inc_by *= -1;
+            }   
             else if(c == 'A'){
                 set_inc_by_timer(inc_by); 
+                digit_num = 0;
+                if(state == 1){
+                    new_state = 1;
+                }
                 state = 3;
             }
         }
         else if(state == 3 && c == 'B'){
             set_inc_by_timer(0);
             state = 2;
+            new_state = 1;
         }
     }
 }
@@ -153,16 +160,17 @@ int main()
     Column3.rise(&column3_isr);
 
     LCD.begin();
-    set_timer(0, 30);
+    set_timer(10, 30);
 
     /* enable ports (DEF) */
-    gpio_enable((char*)"DEF");
+    gpio_enable((char*)"ADEF");
 
     /* set MODER */
-    gpio_moder(GPIOE, 13, GPIO_OUTPUT);     // set PE_13 to output
-    gpio_moder(GPIOF, 15, GPIO_OUTPUT);     // set PF_15 to output
-    gpio_moder(GPIOD, 8,  GPIO_OUTPUT);     // set PD_8 to output
-    gpio_moder(GPIOD, 9,  GPIO_OUTPUT);     // set PD_9 to output
+    gpio_moder(GPIOE, 13, GPIO_OUTPUT);     // set PE_13 for row 4
+    gpio_moder(GPIOF, 15, GPIO_OUTPUT);     // set PF_15 for row 3
+    gpio_moder(GPIOD, 8,  GPIO_OUTPUT);     // set PD_8  for row 2
+    gpio_moder(GPIOD, 9,  GPIO_OUTPUT);     // set PD_9  for row 1
+    gpio_moder(GPIOA, 5,  GPIO_OUTPUT);     // set PA_5  for blue LED
 
     while (true) {
 
@@ -171,48 +179,75 @@ int main()
                 LCD.clear();
                 LCD.setCursor(2, 0);
                 LCD.print("Enter a time:");
-                LCD.setCursor(6, 1);
+                LCD.setCursor(TIMER_LOC, 1);
 
                 LCD.print("m:ss");
                 new_state = 0;
             }
-            else if(get_key()[0] != 0 && digit_num < 3){
-                LCD.setCursor(timer_cursor, 1);
-                LCD.print(get_key());
-
-                if(timer_cursor == 6)   {timer_cursor+=2;}
-                else if(timer_cursor<9) {timer_cursor++; }
+            // else if(get_key()[0] != 0 && digit_num < TIMER_MAX_LEN){
+            else if(get_key()[0] != 0){
+                LCD.setCursor(TIMER_LOC, 1);
+                LCD.print(output_press_timer());
 
                 digit_num++;
                 set_key(0);
             }
-            if(digit_num == 3){
-                decode_timer();
-                new_state = 1;
-                state = 2;
+            if(c_flag){
+                LCD.setCursor(15, 1);
+                if(inc_by == 1){
+                    LCD.print("^");
+                }
+                else{
+                    LCD.print("v");
+                }
+                c_flag = 0;
             }
         }
         else if(state == 2){
             if (new_state){
+                LCD.clear();
                 LCD.setCursor(0, 0);
-                LCD.print("Press 'A' or 'C'");
+                LCD.print("Paused:");
+
+                LCD.setCursor(TIMER_LOC, 1);
+                LCD.print(string_timer());
+
                 new_state = 0;
             }
         }
         else if(state == 3){
+            if(new_state){
+                int_timer();
+                if(inc_by == 1){
+                    swap_timer();
+                }
+                new_state = 0;
+            }
             if(timer_flag){            
                 LCD.clear();
-
                 LCD.setCursor(0, 0);
-                LCD.print("Time Remaining:");
+
+                if(inc_by == 1){
+                    LCD.print("Time Passed:");
+                }
+                else{
+                    LCD.print("Time Remaining:");
+                }
                 LCD.setCursor(6, 1);
                 LCD.print(string_timer());
                 timer_flag = 0;
 
                 if(goal_timer()){
                     LCD.clear();
-                    LCD.setCursor(4, 0);
-                    LCD.print("Times Up");
+                    if(inc_by == 1){
+                        LCD.setCursor(2, 0);
+                        LCD.print("Time Reached");
+                    }
+                    else{
+                        LCD.setCursor(4, 0);
+                        LCD.print("Times Up");
+                    }
+                    reset_timer();
                 }
             }
         }
@@ -223,11 +258,12 @@ int main()
                 LCD.print("Press 'D' to");
                 LCD.setCursor(2, 1);
                 LCD.print("enter a time");
+                new_state = 0;
             }
         }
 
-        set_row_keypad(&++row); // (also sleeps for 10ms)
-        delay_keypad(&press_pause, &press_flag);
+        set_row_keypad(&++row);                     // (also sleeps for 10ms)
+        delay_keypad(&press_pause, &press_flag);    // activates/deactivates blue LED
     }
 }
 
