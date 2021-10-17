c67558e94be753505e44408d8c676ab11dd397b9
jesseboth <jessebot@buffalo.edu>
2021-10-16 20:55:03 -0400

    Added a way to interpret data from keypad

--- Project 2/Project 2/CSE321_project2_jessebot_keypad.cpp
+++ Project 2/Project 2/CSE321_project2_jessebot_keypad.cpp
@@ -4,14 +4,62 @@ char keys[4][4] = { {'1', '2', '3', 'A'},
                     {'4', '5', '6', 'B'},
                     {'7', '8', '9', 'C'},
                     {'*', '0', '#', 'D'} };
-
-char check_keypad(char row){
-    check_column_keypad();
-
-    return 'a';    
+char key[2];
+char get_char_keypad(char row, char col){
+    printf("%c\n", keys[row][col]);
+    return keys[row][col];    
 }
 
-char check_column_keypad(){
+void set_row_keypad(char *row){
+    // pin 5 - PE_13 -> row 4
+    // pin 6 - PF_15 -> row 3
+    // pin 7 - PD_8  -> row 2
+    // pin 8 - PD_9  -> row 1
 
-    return 'a';
+    if(*row == 0){
+        gpio_off(GPIOE, 13);
+        gpio_off(GPIOF, 15);
+        gpio_off(GPIOD, 8);
+        gpio_on(GPIOD, 9);
+        thread_sleep_for(10);
+    }
+    else if(*row == 1){
+        gpio_off(GPIOE, 13);
+        gpio_off(GPIOF, 15);
+        gpio_on(GPIOD, 8);
+        gpio_off(GPIOD, 9);
+        thread_sleep_for(10);
+    }
+    else if(*row == 2){
+        gpio_off(GPIOE, 13);
+        gpio_on(GPIOF, 15);
+        gpio_off(GPIOD, 8);
+        gpio_off(GPIOD, 9);
+        thread_sleep_for(10);
+    }
+    else {
+        gpio_on(GPIOE, 13);
+        gpio_off(GPIOF, 15);
+        gpio_off(GPIOD, 8);
+        gpio_off(GPIOD, 9);
+        thread_sleep_for(10);
+        *row=-1;
+    }
+}
+
+void delay_keypad(char *pause, char *flag){
+    if(*pause>0){
+        (*pause)--;
+    }
+    else{
+        *flag = 1;
+    }
+}
+
+void set_key(char c){
+    key[0] = c;
+}
+
+char *get_key(){
+    return key;
 }
--- Project 2/Project 2/CSE321_project2_jessebot_keypad.h
+++ Project 2/Project 2/CSE321_project2_jessebot_keypad.h
@@ -1,3 +1,26 @@
+#include "mbed.h"
+#include "CSE321_project2_jessebot_gpio.h"
 
-char check_keypress();
-char check_column_keypad();
+/*
+    Keypad Matrix Config
+
+    COL
+    pin 1 - PF_13
+    pin 2 - PE_9
+    pin 3 - PE_11
+    pin 4 - PF_14
+    
+    ROW
+    pin 5 - PE_13
+    pin 6 - PF_15
+    pin 7 - PD_8
+    pin 8 - PD_9
+
+*/
+#define PAUSE_FOR 25 //*10 ms
+
+char get_char_keypad(char row, char col);
+void set_row_keypad(char *row);
+void delay_keypad(char *pause, char *flag);
+void set_key(char c);
+char *get_key();
