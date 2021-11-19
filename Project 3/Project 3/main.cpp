0d94d33e6f746992bebe78fa0cfdb101d8f8b5b8
jesseboth <jessebot@buffalo.edu>
2021-11-18 22:25:13 -0500

    main created

---
+++ Project 3/Project 3/main.cpp
@@ -0,0 +1,40 @@
+/**
+ * Author: Jesse Both
+ * Date: 11/19/2021
+ * 
+ * Assignment:  Project 3
+ *              Alternative Hearing Device
+ * 
+ * Purpose:     To provide an alternative form of hearing
+ *              for the hearing impaired.
+ * 
+ * Inputs: 
+ *          - Audio Transducer
+ *          - Rotary Encoder
+ *          - Onboard Button
+ * Outputs: 
+ *          - LEDs
+ *          - Vibration Motor
+ * 
+ * Constraints:
+ *          - Detect sound and will stimulate an alternative sense rather than sound.
+ *          - Provide a visual and physical que that noise is nearby.
+ *          -  Desired sound sensitivity can be adjusted.
+ *          - Toggle on and off the device.
+ * 
+ * Subroutines:
+ *              main()
+ * 
+ * Sources:
+ * 
+*/
+
+#include "mbed.h"
+
+int main()
+{
+    while (true) {
+
+    }
+}
+
