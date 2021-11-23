8ee853e6810434edf1cc74f3a665d738dab58f33
jesseboth <jessebot@buffalo.edu>
2021-11-23 13:14:01 -0500

    rotary encoder functionality

--- Project 3/Project 3/CSE321_project3_jessebot_main.cpp
+++ Project 3/Project 3/CSE321_project3_jessebot_main.cpp
@@ -38,16 +38,21 @@
 #define LCD_COLS 16
 #define LCD_ROWS 2
 #define MAX_LVL 10
+#define BUTTON_DELAY 5
 
 CSE321_LCD LCD(LCD_COLS, LCD_ROWS, LCD_5x10DOTS, PB_9, PB_8);
 EventQueue EQ(32 * EVENTS_EVENT_SIZE);
 Thread THREAD;
-InterruptIn pwr(BUTTON1); // pwr button for device
-DigitalOut pwr_ind(LED1);      // on/off indicator
-
+Ticker TICK;
+InterruptIn pwr(PD_5, PullUp);        // pwr button for device
+DigitalOut pwr_ind(LED1);               // on/off indicator
 
 char BUTTON_flag = 1;       // flag to debounce user button
+char but_delay = 0;
 char ON_flag = 1;           // flag determine if the system is reading audio
+char ROTARY_flag = 1;       // flag to debounce rotary encoder
+
+int volume = 2;
 
 /* #region  TESTING */
 int here=0;
@@ -65,62 +70,77 @@ void set_lvl(int l, int row, char *str){
     LCD.clear_until(MAX_LVL - l);
 }
 
+void BUTTON_debounce(){
+    if(!BUTTON_flag && !but_delay){
+        BUTTON_flag = 1;
+    }
+    else{
+        but_delay--;
+    }
+}
+
 void BUTTON_rise_handler(){  
     if(BUTTON_flag){
         ON_flag ^= 1;
         pwr_ind = !pwr_ind;
-        EQ.call(printf, "HERE %d\n", here++);
+        but_delay = BUTTON_DELAY;
         BUTTON_flag = 0;
     }
 }
 
-void BUTTON_fall_handler(){
-    BUTTON_flag = 1;
-}
+/*
+PD_7: CLk
+PD_6: DT
+PD_5: SW
+*/
 
 int main(){
-    int x = 0;
-    int flag = 0;
+    char rot_clk, rot_dt, rot_clk_str, rot_dt_str;
     pwr_ind = 1;
     THREAD.start(callback(&EQ, &EventQueue::dispatch_forever));
-    pwr.rise(BUTTON_rise_handler);
-    pwr.fall(EQ.event(BUTTON_fall_handler));
+    pwr.rise(EQ.event(BUTTON_rise_handler));
+    TICK.attach(EQ.event(BUTTON_debounce), 50ms);
+
+    gpio_enable((char *)"D");
+    gpio_moder(GPIOD, 7, GPIO_INPUT);       // rotary CLK
+    gpio_moder(GPIOD, 6, GPIO_INPUT);       // rotary DT
 
 
     LCD.begin();
-    set_lvl(5, 1, (char *)"vol:");
+    set_lvl(3, 0, (char *)"lvl:");
+    set_lvl(volume, 1, (char *)"vol:");
 
     while (true){
-
-        /* #region TESTING */
         if(ON_flag){
             if(!LCD.displayCheck()){
                 LCD.displayON();
             }
-            set_lvl(x, 0, (char *)"lvl:");
-            thread_sleep_for(50);
+            /* #region ROTARY */
+            rot_clk = gpio_check(GPIOD, 7);
+            rot_dt = gpio_check(GPIOD, 6);
 
+            if(ROTARY_flag && !rot_clk){
+                rot_clk_str = rot_clk;
+                rot_dt_str = rot_dt;
+                ROTARY_flag = 0;
 
-            if (x == MAX_LVL){
-                flag = 1;
+                if(rot_clk == rot_dt && volume>0){
+                    volume--;
+                }
+                else if(volume<MAX_LVL){
+                    volume++;
+                }
+                set_lvl(volume, 1, (char *)"vol:");
             }
-            else if (x == 0){
-                flag = 0;
+            else if(!ROTARY_flag && rot_clk && rot_dt){
+                ROTARY_flag = 1;
             }
-
-            if (flag){
-                x--;
-            }
-            else{
-                x++;
-            }
+            /* #endregion ROTARY */
         }
         else{
             if(LCD.displayCheck()){
                 LCD.displayOFF();
             }
         }
-
-        /* #endregion */
     }
 }
